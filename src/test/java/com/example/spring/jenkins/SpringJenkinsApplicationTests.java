7d8ae0da7352387aec0e1c1ab95d328ecba880cd
Singh <manish.h.singh@capgemini.com>
2023-08-30 15:13:46 +0530

    first commit

---
+++ src/test/java/com/example/spring/jenkins/SpringJenkinsApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.spring.jenkins;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJenkinsApplicationTests {
+	public static Logger logger = LoggerFactory.getLogger(SpringJenkinsApplicationTests.class);
+	@Test
+	void contextLoads() {
+		//assertEquals(true,true);
+		logger.info("test case execution...");
+	}
+
+}
